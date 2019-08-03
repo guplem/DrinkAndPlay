bd274faeb20a012cc4f5fafaa28f423fa5258f36
guplem <guplem@gmail.com>
2019-08-03 13:38:40 +0200

    CSV Parsing ok

---
+++ DrinkAndPlay/Assets/Scripts/CSVReader.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class CSVReader
+{
+    static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+    static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+    static char[] TRIM_CHARS = { '\"' };
+
+    public static List<Dictionary<string, object>> Read(string file)
+    {
+        var list = new List<Dictionary<string, object>>();
+        TextAsset data = Resources.Load(file) as TextAsset;
+
+        var lines = Regex.Split(data.text, LINE_SPLIT_RE);
+
+        if (lines.Length <= 1) return list;
+
+        var header = Regex.Split(lines[0], SPLIT_RE);
+        for (var i = 1; i < lines.Length; i++)
+        {
+
+            var values = Regex.Split(lines[i], SPLIT_RE);
+            if (values.Length == 0 || values[0] == "") continue;
+
+            var entry = new Dictionary<string, object>();
+            for (var j = 0; j < header.Length && j < values.Length; j++)
+            {
+                string value = values[j].Replace("\"\"", "\"");
+                value = UnquoteString(value);
+                value = value.Replace("\\", "");
+                object finalvalue = value;
+                int n;
+                float f;
+                if (int.TryParse(value, out n))
+                {
+                    finalvalue = n;
+                }
+                else if (float.TryParse(value, out f))
+                {
+                    finalvalue = f;
+                }
+                entry[header[j]] = finalvalue;
+            }
+            list.Add(entry);
+        }
+        return list;
+    }
+
+    public static string UnquoteString(string str)
+    {
+        if (String.IsNullOrEmpty(str))
+            return str;
+
+        int length = str.Length;
+        if (length > 1 && str[0] == '\"' && str[length - 1] == '\"')
+            str = str.Substring(1, length - 2);
+
+        return str;
+    }
+}
--- DrinkAndPlay/Assets/Scripts/Managers/LocalizationManager.cs
+++ DrinkAndPlay/Assets/Scripts/Managers/LocalizationManager.cs
@@ -20,24 +20,44 @@ public class LocalizationManager
     public bool LoadLanguage(string lang, string textsType)
     {
 
-        if (string.IsNullOrEmpty(lang))
+        /*if (string.IsNullOrEmpty(lang))
         {
             Debug.LogError("The desired language is null or empty");
             return false;
-        }
+        }*/
 
 
+        /*
         TextAsset localizationFile = Resources.Load<TextAsset>("LocalizationFile.csv");
         string[] linesFromLocalizationFile = localizationFile.text.Split("\n"[0]);
         foreach (string line in linesFromLocalizationFile)
         {
             Debug.Log("LINE KKK: " + line);
         }
+        */
 
         //Open file
         //Foreach line
         //Save the line as localizated text map
 
+
+        List<Dictionary<string, object>> data = CSVReader.Read("LocalizationFile");
+
+        for (var i = 0; i < data.Count; i++)
+        {
+            Debug.Log("Type '" + data[i]["Type"] + "'");
+            Debug.Log("Id '" + data[i]["Id"] + "'");
+            Debug.Log("NaughtyLevel '" + data[i]["NaughtyLevel"] + "'");
+            Debug.Log("es-es '" + data[i]["es-es"] + "'");
+            Debug.Log("en-us '" + data[i]["en-us"] + "'");
+            Debug.Log("================================");
+        }
+
+
+
+
         return true;
     }
+
+    
 }

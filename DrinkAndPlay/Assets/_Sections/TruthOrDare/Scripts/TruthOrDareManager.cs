c36d182cb1a1c72200af6dacde608a23510db89e
guplem <guplem@gmail.com>
2019-09-27 13:52:49 +0200

    Bases for truth or dare

---
+++ DrinkAndPlay/Assets/_Sections/TruthOrDare/Scripts/TruthOrDareManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruthOrDareManager : TurnsGameManager
+{
+    public override void NextButton()
+    {
+        
+    }
+
+    public override void PreviousButton()
+    {
+        
+    }
+}

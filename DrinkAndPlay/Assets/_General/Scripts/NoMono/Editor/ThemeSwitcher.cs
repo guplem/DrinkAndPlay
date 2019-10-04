9e8b9450a4222fa3874fe6bb2c55b36ab3852e33
guplem <guplem@gmail.com>
2019-10-04 03:48:33 +0200

    Editor theme switcher

---
+++ DrinkAndPlay/Assets/_General/Scripts/NoMono/Editor/ThemeSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ThemeSwitcher : MonoBehaviour
+{
+    public static LightDarkColor.ColorType currentTheme = LightDarkColor.ColorType.Dark;
+    
+    [MenuItem("Drink and Play/Switch Theme %&d")]
+    public static void SwitchTheme()
+    {
+        currentTheme = (currentTheme == LightDarkColor.ColorType.Light)
+            ? LightDarkColor.ColorType.Dark
+            : LightDarkColor.ColorType.Light;
+        
+        ColorSwitcher[] foundObjects = FindObjectsOfType<ColorSwitcher>();
+        foreach (ColorSwitcher obj in foundObjects)
+            obj.SetColorTo(currentTheme);
+        
+    }
+    
+}

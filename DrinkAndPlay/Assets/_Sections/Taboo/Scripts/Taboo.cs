46013546a137e979a0fcd122f8332810c35ecaf3
Guillem Poy <guplem@gmail.com>
2020-07-02 16:48:09 +0200

    Taboo bases

---
+++ DrinkAndPlay/Assets/_Sections/Taboo/Scripts/Taboo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taboo : TurnsGameManager
+{
+    public override void NextButton()
+    {
+        
+    }
+
+    public override void PreviousButton()
+    {
+        
+    }
+}

a35ffdb7ecd8dd458d46b41269c0a165acd66b1b
Guillem Poy <guplem@gmail.com>
2020-07-02 17:31:32 +0200

    Working basics

--- DrinkAndPlay/Assets/_Sections/Taboo/Scripts/Taboo.cs
+++ DrinkAndPlay/Assets/_Sections/Taboo/Scripts/Taboo.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class Taboo : TurnsGameManager
 {
+    
+    [SerializeField] private Localizer turnText;
+    [SerializeField] private GameObject[] playerTurnElements;
+    [SerializeField] private GameObject[] tabooGameElements;
+    
     public override void NextButton()
     {
+        gm.dataManager.SetTurnForNextPlayer();
         
+        if (AreWeOnTopHistory())
+        {
+            turnText.Localize(); //Update player
+
+            SetActiveTurnElements(true);
+            SetActiveTabooGameElements(false);
+        }
+        else
+        {
+            SetupTextInCard(GetNextText(false));
+        }
     }
 
     public override void PreviousButton()
     {
+        gm.dataManager.PreviousPlayerTurn();
+        SetupTextInCard(GetPreviousText());
+    }
+    
+    private void SetActiveTabooGameElements(bool state)
+    {
+        foreach (GameObject go in tabooGameElements)
+            go.SetActive(state);
+    }
+    
+    private void SetActiveTurnElements(bool state)
+    {
+        foreach (GameObject go in playerTurnElements)
+            go.SetActive(state);
+    }
+    
+    public void PlayerAccepted()
+    {
+        SetActiveTurnElements(false);
+        SetActiveTabooGameElements(true);
         
+        SetupTextInCard(GetNextText(false));
     }
 }
